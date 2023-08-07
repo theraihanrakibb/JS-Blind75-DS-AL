fc6c1e27324eaeb24a6ca8acc8efabd44f1ef982
Raihan <raihanrakib.143@gmail.com>
2023-08-08 05:26:25 +0800

    First-Commit

---
+++ Arrays & Hashing/Two-sum.js
@@ -0,0 +1,16 @@
+const twoSum = function(nums, target) {
+
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++){
+
+    const compliment = target - nums[i];
+    if(map.has(compliment)){
+        return[i, map.get(compliment)];
+    }else{
+        map.set(nums[i], i);
+    }
+
+  }
+
+};
